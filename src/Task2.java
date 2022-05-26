dec371b8e1e18cab66dda856f9f921684fe0f720
Ruslan.Shine <ruslan.shainuroff@yandex.ru>
2022-05-26 21:40:48 +0500

    готовый

--- src/Task2.java
+++ src/Task2.java
@@ -5,7 +5,5 @@ public class Task2 {
         System.out.println(motorbike.getModel() + "\n" +
                 motorbike.getYear() + "\n" +
                 motorbike.getColor());
-
-        System.out.println("Hello");
     }
 }
